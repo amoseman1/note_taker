2271428e30fc1cfea9a96dca1ad7fc62c9b10027
Waterbug <amoseman18@gmail.com>
2020-12-15 22:24:00 -0700

    fixed routes comments

--- Develop/routes/api_routes.js
+++ Develop/routes/api_routes.js
@@ -2,12 +2,7 @@ const router = require("express").Router();
 const dbjson = require('../db/db.json');
 const fs = require('fs');
 
-
-
-
-///rest of code gies here
-//twoi routes- get and post, both /notes
-//fs.readfile --> the db_.json file, take in the data in a .then function, res.json(data)
+//Get route handler
 router.get('/notes', (req, res) =>
     fs.readFile('db/db.json', "utf8", (err, notes) => {
         if (err) {
@@ -20,12 +15,9 @@ router.get('/notes', (req, res) =>
 
     }));
 
-//post route, fs read file, read db json file, store in an array, add tthe new note to that array, 
-//then write to db.json, then do res.json with updated array
+//Post route handler
 router.post('/notes', (req, res) => {
     let notes = req.body
-    //aftger you get notes ^ 
-    // you'll need to attach a key value par of "id" : "randomstringofletttersornumbers"
     fs.readFile('db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err)
@@ -45,19 +37,6 @@ router.post('/notes', (req, res) => {
 
 
     });
-
-    // //console.log(dbjson);
-
-    // fs.writeFile('db/db.json', JSON.stringify(newerNote), err => {
-    //     if (err) {
-    //         console.error(err)
-    //         return
-    //     }
-    //     res.json(newNoteArray)
-    //     console.log('File written successfully!');
-
-    // });
-
 });
 
 module.exports = router
